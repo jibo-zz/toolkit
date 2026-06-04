fbb54ee4dddaf5462a6569764411eb5d67616a4f
Mohamed Omar Cheikh <mohamed.omarcheikh@gmail.com>
2026-06-03 22:32:31 -0400

    add DownloadStaticFile method with test

--- tools.go
+++ tools.go
@@ -172,3 +172,13 @@ func (t *Tools) Slugify(s string) (string, error) {
 	}
 	return slug, nil
 }
+
+// DownloadStaticFile downloads a file, and tries to force the browser to avoid displaying it
+// in the browser window by setting the Content-Disposition. It also allows specification of
+// the display name.
+func (t *Tools) DownloadStaticFile(w http.ResponseWriter, r *http.Request, path, file, displayName string) {
+	fullPath := filepath.Join(path, file)
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", displayName))
+
+	http.ServeFile(w, r, fullPath)
+}
